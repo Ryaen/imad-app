2f97fada81dbb76bdaf9a996c7ac4fd4d2392937
Shubham Giri <35848029+Ryaen@users.noreply.github.com>
2018-02-21 18:46:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,6 +110,9 @@ app.get('/articlethree', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
