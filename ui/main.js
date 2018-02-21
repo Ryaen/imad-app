61572814b434accdc5688b06751b6b9afb358e0e
Shubham Giri <35848029+Ryaen@users.noreply.github.com>
2018-02-21 20:39:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var element = document.getElementById('maintext');
+/*var element = document.getElementById('maintext');
 element.innerHTML='New value';
 var img = document.getElementById('modi');
 var marginLeft = 0;
@@ -9,4 +9,19 @@ function moveright(){
 }
 img.onclick=function(){
    var interval = setInterval(moveright,200);
-    };
+    };*/
+var button = document.getElementById('counter');
+button.onclick = function(){
+
+    //make a request tocounter endpoint
+    
+    
+    //cature the response and store it in a variable
+    
+    
+    //render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
+};
