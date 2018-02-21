1d32b62e07674141b773ebababa8ef8c77e76d09
Shubham Giri <35848029+Ryaen@users.noreply.github.com>
2018-02-21 22:52:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,3 +32,17 @@ button.onclick = function(){
     request.open('GET','http://shubhgiri345.imad.hasura-app.io/counter',true);
     request.send(null);
  };
+ //submit name
+ var nameinput = document.getElementById("name");
+ var name = nameinput.value;
+ var submit = document.getElementById("submit_bin");
+ submit.onclick = function(){
+  var names : {'name1','name2','name3'};
+var list = '';
+for(var i=0;i<names.length;i++)
+ list += '<li>' + names[i] + '</li>';
+ var ul = document.getElementById("namelist");
+ ul.innerHTML = list;
+ };
+
+ }
