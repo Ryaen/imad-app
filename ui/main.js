cc8a6d5e53fcbc3dd9d249b3e75df77fc18723b6
Shubham Giri <35848029+Ryaen@users.noreply.github.com>
2018-02-21 20:58:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,15 +13,22 @@ img.onclick=function(){
 var button = document.getElementById('counter');
 button.onclick = function(){
 
-    //make a request tocounter endpoint
-    
+    //create a request to counter endpoint
+    var request = new XMLHttpRequest();
     
     //cature the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+            
+        }
+    };
     
-    
-    //render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
-};
+    //make a request
+    request.open('GET','http://shubhgiri345.imad.hasura-app.io/counter',true);
+    request.send(null);
+ };
