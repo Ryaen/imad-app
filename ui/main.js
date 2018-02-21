91bb1abeab870221128310eb81accfafb0fcf2d2
Shubham Giri <35848029+Ryaen@users.noreply.github.com>
2018-02-21 22:52:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ button.onclick = function(){
  var name = nameinput.value;
  var submit = document.getElementById("submit_bin");
  submit.onclick = function(){
-  var names : {'name1','name2','name3'};
+  var names : {'name1','name2','name3','name4'};
 var list = '';
 for(var i=0;i<names.length;i++)
  list += '<li>' + names[i] + '</li>';
