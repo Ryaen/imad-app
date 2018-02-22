8d7ca4d41bd906c5f3ef57834110843eb378008f
Shubham Giri <35848029+Ryaen@users.noreply.github.com>
2018-02-22 20:49:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,12 +37,27 @@ button.onclick = function(){
  var name = nameinput.value;
  var submit = document.getElementById("submit_bin");
  submit.onclick = function(){
-  var names = ['name1','name2','name3','name4'];
-var list = '';
-for(var i=0;i<names.length;i++){
- list += '<li>' + names[i] + '</li>';
-}
- var ul = document.getElementById("namelist");
- ul.innerHTML = list;
- };
+     var request = new XMLHttpRequest();
+    
+    //cature the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                 var names = request.responseText;
+                 names = JSON.parse(names);=
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                list += '<li>' + names[i] + '</li>';
+                    }
+                var ul = document.getElementById("namelist");
+                ul.innerHTML = list;
+                }
+            }
+            
+        }
+        
+         request.open('GET','http://shubhgiri345.imad.hasura-app.io/submit?name=' + name,true);
+         request.send(null);
+    };
+ 
  
